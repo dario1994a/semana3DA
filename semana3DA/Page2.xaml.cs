2ff8f0c82970ff93ac1c9f943e349050b040585d
master <master@SMLAB306PC17_23>
2023-06-12 21:06:21 -0500

    Agregar archivos de proyecto.

---
+++ semana3DA/Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana3DA
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string dato1, double dato2)
+        {
+            InitializeComponent();
+            lblDato1.Text = "DATO UNO" +dato1;
+            lblDato2.Text = "DATO DOS" + dato2.ToString();
+
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
